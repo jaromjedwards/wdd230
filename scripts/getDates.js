d42607344e4f11995fcbf1796b55a9ad0eff207d
Jarom Edwards <jaromjedwards@gmail.com>
2023-11-06 20:36:11 -0700

    week 1

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,14 +1,22 @@
 window.onload = function() {
     console.log('Page is loaded');
+    // call functions
     getYear();
+    lastModified();
   };
   
   
-  function getYear() {
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
-  
-    const yearElement = document.getElementById('year');
-    yearElement.innerText = currentYear;
-  }
-  
+function getYear() {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+
+  const yearElement = document.getElementById('year');
+  yearElement.innerText = `©${currentYear} Jarom Edwards - Arizona`;
+}
+
+function lastModified(){
+  let lastModified = new Date(document.lastModified);
+  let date = lastModified.toLocaleDateString();
+  const lastModifiedElement = document.getElementById('lastModified');
+  lastModifiedElement.innerText = `Last Modified: ${date}`;
+}
